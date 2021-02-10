a4057bb49366adbc41a0c625141cc472f65d007b
Michael Linthicum <mplinthicum@gmail.com>
2021-02-09 20:26:01 -0600

    exercises 4.2 to 4.5 more options

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -27,3 +27,37 @@ sealed trait Option[+A] {
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Option {
+
+  // 4.2
+  def variance(xs: Seq[Double]): Option[Double] = {
+    def mean(x: Seq[Double]): Option[Double] = if (x.isEmpty) None else Some(x.sum / x.length)
+    mean(xs).flatMap(m => mean(xs.map(i => math.pow(i - m, 2))))
+  }
+
+  // 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(x => b.map(y => f(x, y)))
+  }
+
+  // 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(a => sequence(t).map(a :: _))
+  }
+
+  // 4.5
+  def naiveTraverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    sequence(a.map(f))
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => f(h).flatMap(a => traverse(t)(f).map(a :: _))
+  }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)(x => x)
+  }
+}
--- src/test/scala/fpinscala/errorhandling/OptionSpec.scala
+++ src/test/scala/fpinscala/errorhandling/OptionSpec.scala
@@ -1,5 +1,6 @@
 package fpinscala.errorhandling
 
+import Option._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -60,6 +61,73 @@ class OptionSpec extends AnyFlatSpec with Matchers {
   it should "return None if None" in {
     val x: Option[Int] = None
     x.filter(_ % 2 == 0) should be(None)
+  }
+
+  "variance" should "return the variance of a sequence" in {
+    variance(Seq(1.0, 1.0, 2.0, 2.0)) should be(Some(0.25))
+  }
+
+  it should "return None for an empty dataset" in {
+    variance(Seq()) should be(None)
+  }
+
+  it should "return 0 for all equal numbers" in {
+    variance(Seq(1, 1, 1, 1)) should be(Some(0.0))
+  }
+
+  it should "return 0 for a sequence of length 1" in {
+    variance(Seq(1)) should be(Some(0.0))
+  }
+
+  it should "return 0 for a list of 0s" in {
+    variance(Seq(0, 0, 0)) should be(Some(0.0))
+  }
+
+  def addToString(x: Int, y: Int): String = (x + y).toString
+
+  "map2" should "lift a function" in {
+    def x: Option[Int] = Some(1) // "1".toInt
+    def y: Option[Int] = Some(2) // "2".toInt
+
+    map2(x, y)(addToString) should be(Some("3"))
+  }
+
+  it should "lift a function and return none" in {
+    def x: Option[Int] = Some(1) // "1".toInt
+    def y: Option[Int] = None // "fish".toInt
+
+    map2(x, y)(addToString) should be(None)
+  }
+
+  "sequence" should "transform a list of options into an optional list" in {
+    sequence(List(Some(1), Some(2), Some(3))) should be(Some(List(1, 2, 3)))
+  }
+
+  it should "return None if one of the items is None" in {
+    sequence(List(Some(1), None, Some(3))) should be(None)
+  }
+
+  def smallStringToInt(a: String): Option[Int] = a match {
+    case "1" => Some(1)
+    case "2" => Some(2)
+    case "3" => Some(3)
+    case _ => None
+  }
+
+  "traverse" should "transform a list of strings to ints" in {
+    traverse(List("1", "2", "3"))(smallStringToInt) should be(Some(List(1, 2, 3)))
+  }
+
+  it should "return none" in {
+    traverse(List("1", "50", "3"))(smallStringToInt) should be(None)
+  }
+
+  "sequenceViaTraverse" should "transform a list of options into an optional list" in {
+    sequenceViaTraverse(List(Some(1), Some(2), Some(3))) should be(Some(List(1, 2, 3)))
+  }
+
+  it should "return None if one of the items is None" in {
+    sequenceViaTraverse(List(Some(1), None, Some(3))) should be(None)
   }
 
 }
